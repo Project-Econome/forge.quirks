8be14bcab450d5e8398a5a905d298bebfe2c2533
Dark-LYNN <botdevlynn@gmailcom>
2024-12-30 01:33:10 +0100

    Added: cpuTemperature

--- dist/functions/system/cpuTemp.js
+++ dist/functions/system/cpuTemp.js
@@ -7,14 +7,15 @@ const forgescript_1 = require("@tryforge/forgescript");
 const systeminformation_1 = __importDefault(require("systeminformation"));
 exports.default = new forgescript_1.NativeFunction({
     name: "$cpuTemp",
+    aliases: ['$cpuTemperature'],
     version: '1.0.0',
-    description: "Returns the cpu temperature.",
+    description: "Returns the cpu temperature in celsius.",
     output: forgescript_1.ArgType.Number,
     unwrap: false,
     async execute() {
         const cpuTemp = await systeminformation_1.default.cpuTemperature();
         const mainTemp = cpuTemp.main;
-        return this.success(mainTemp);
+        return this.success(mainTemp + '°C');
     },
 });
 //# sourceMappingURL=cpuTemp.js.map
--- src/functions/system/cpuTemp.ts
+++ src/functions/system/cpuTemp.ts
@@ -3,8 +3,9 @@ import si from "systeminformation";
 
 export default new NativeFunction({
   name: "$cpuTemp",
+  aliases: ['$cpuTemperature'],
   version: '1.0.0',
-  description: "Returns the cpu temperature.",
+  description: "Returns the cpu temperature in celsius.",
   output: ArgType.Number,
   unwrap: false,
 
@@ -12,6 +13,6 @@ export default new NativeFunction({
     const cpuTemp = await si.cpuTemperature();
     const mainTemp = cpuTemp.main;
 
-    return this.success(mainTemp);
+    return this.success(mainTemp + '°C');
   },
 });

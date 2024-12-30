9292367982781aed99849cf4e4290042a0bfe392
Dark-LYNN <botdevlynn@gmailcom>
2024-12-30 21:13:44 +0100

    Added: ramTotal

--- dist/types.js
+++ dist/types.js
@@ -1,3 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.FileUnit = void 0;
+var FileUnit;
+(function (FileUnit) {
+    FileUnit[FileUnit["B"] = 0] = "B";
+    FileUnit[FileUnit["KB"] = 1] = "KB";
+    FileUnit[FileUnit["MB"] = 2] = "MB";
+    FileUnit[FileUnit["GB"] = 3] = "GB";
+    FileUnit[FileUnit["TB"] = 4] = "TB";
+})(FileUnit || (exports.FileUnit = FileUnit = {}));
 //# sourceMappingURL=types.js.map
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
-export declare enum FileUnit {
-  B = 0,
-  KB = 1,
-  MB = 2,
-  GB = 3,
-  TB = 4
+export enum FileUnit {
+  B,
+  KB,
+  MB,
+  GB,
+  TB
 }

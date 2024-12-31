74ca6c3ce810bdfcd511e75e3a3c8808899fdd0a
Dark-LYNN <botdevlynn@gmailcom>
2024-12-31 20:07:27 +0100

    Added shardsIdle

---
+++ src/functions/bot/shardsIdle.ts
@@ -0,0 +1,16 @@
+import { ArgType, NativeFunction } from "@tryforge/forgescript";
+import { Status } from 'discord.js';
+
+export default new NativeFunction({
+  name: "$shardsIdle",
+  version: '1.0.0',
+  description: "Returns the total amount of idle shards.",
+  output: ArgType.Number,
+  unwrap: false,
+
+  async execute(ctx) {
+    const onlineShards = ctx.client.ws.shards.filter(shard => shard.status === Status.Idle).size;
+  
+    return this.success(onlineShards);
+  },
+});

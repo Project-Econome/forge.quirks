b1d620662c0bcc0a21d14e3175393d31d608896a
Dark-LYNN <botdevlynn@gmailcom>
2024-12-31 18:10:21 +0100

    Added shardsOffline

---
+++ src/functions/bot/shardsOffline.ts
@@ -0,0 +1,16 @@
+import { ArgType, NativeFunction } from "@tryforge/forgescript";
+import { Status } from 'discord.js';
+
+export default new NativeFunction({
+  name: "$shardsOnline",
+  version: '1.0.0',
+  description: "Returns the total amount of offline shards.",
+  output: ArgType.Number,
+  unwrap: false,
+
+  async execute(ctx) {
+    const onlineShards = ctx.client.ws.shards.filter(shard => shard.status === Status.Disconnected).size;
+  
+    return this.success(onlineShards);
+  },
+});

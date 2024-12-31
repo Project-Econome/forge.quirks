6bba8f9559592cb99ace37f326df5de4a52c8bdb
Dark-LYNN <botdevlynn@gmailcom>
2024-12-31 14:44:17 +0100

    Added dbPing

---
+++ dist/functions/database/dbping.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//@ts-ignore
+//@ts-nocheck
+const forgescript_1 = require("@tryforge/forgescript");
+exports.default = new forgescript_1.NativeFunction({
+    name: "$dbping",
+    aliases: ['$dblatency'],
+    version: '1.0.0',
+    description: "Returns the database ping.",
+    output: forgescript_1.ArgType.String,
+    unwrap: false,
+    async execute(ctx) {
+        let identifier = 'user_test_scope';
+        let result = NaN;
+        const start = Date.now();
+        try {
+            await ctx.client.db.get({ identifier });
+            const end = Date.now();
+            result = end - start;
+        }
+        catch (error) {
+            if (error instanceof Error) {
+                console.error('Error reading package.json:', error.message);
+            }
+            else {
+                console.error('Error fetching data:', error);
+            }
+        }
+        return this.success(result);
+    },
+});
+//# sourceMappingURL=dbping.js.map
---
+++ src/functions/database/dbping.ts
@@ -0,0 +1,32 @@
+//@ts-ignore
+//@ts-nocheck
+import { ArgType, NativeFunction } from "@tryforge/forgescript";
+
+export default new NativeFunction({
+  name: "$dbping",
+  aliases: ['$dblatency'],
+  version: '1.0.0',
+  description: "Returns the database ping.",
+  output: ArgType.String,
+  unwrap: false,
+
+  async execute(ctx) {
+    let identifier = 'user_test_scope';
+    let result = NaN
+
+    const start = Date.now();
+    try {
+      await ctx.client.db.get({ identifier });
+
+      const end = Date.now();
+      result = end - start
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error('Error reading package.json:', error.message);
+      } else {
+        console.error('Error fetching data:', error);
+      }
+    }
+    return this.success(result);
+  },
+});

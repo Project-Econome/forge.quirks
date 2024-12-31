f9e37e7f98ad697072fa29b8155e9b5533fc1853
Dark-LYNN <botdevlynn@gmailcom>
2024-12-31 12:57:34 +0100

    Added: projectVersion

---
+++ dist/functions/system/getProjectVersion.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const forgescript_1 = require("@tryforge/forgescript");
+const fs_1 = require("fs");
+const path_1 = require("path");
+exports.default = new forgescript_1.NativeFunction({
+    name: "$projectVersion",
+    version: '1.0.0',
+    description: "Returns the version from your package.json usage.",
+    output: forgescript_1.ArgType.String,
+    brackets: false,
+    unwrap: false,
+    async execute() {
+        let result = '';
+        try {
+            const packageJson = JSON.parse((0, fs_1.readFileSync)((0, path_1.join)(process.cwd(), 'package.json'), 'utf-8'));
+            result = `${packageJson.version}`;
+        }
+        catch (error) {
+            if (error instanceof Error) {
+                console.error('Error reading package.json:', error.message);
+            }
+            else {
+                console.error('Unknown error occurred:', error);
+            }
+            result = 'undefined';
+        }
+        return this.success(result);
+    },
+});
+//# sourceMappingURL=getProjectVersion.js.map
---
+++ src/functions/system/getProjectVersion.ts
@@ -0,0 +1,30 @@
+import { ArgType, NativeFunction } from "@tryforge/forgescript";
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+export default new NativeFunction({
+  name: "$projectVersion",
+  version: '1.0.0',
+  description: "Returns the version from your package.json usage.",
+  output: ArgType.String,
+  brackets: false,
+  unwrap: false,
+
+  async execute() {
+    let result = '';
+    try {
+      const packageJson = JSON.parse(readFileSync(join(process.cwd(), 'package.json'), 'utf-8'));
+      result = `${packageJson.version}`;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error('Error reading package.json:', error.message);
+      } else {
+        console.error('Unknown error occurred:', error);
+      }
+      result = 'undefined';
+    }
+
+    return this.success(result);
+  },
+});
+

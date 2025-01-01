fab423de6b580a1378aab1c057757c8a0d79ec4c
Dark-LYNN <botdevlynn@gmailcom>
2025-01-01 13:50:57 +0100

    Added shardInfo

--- src/functions/bot/shardInfo.ts
+++ src/functions/bot/shardInfo.ts
@@ -7,6 +7,7 @@ export default new NativeFunction({
   version: '1.0.0',
   description: "Returns the info on shard.",
   output: ArgType.Number,
+  brackets: true,
   unwrap: true,
   args: [
     {

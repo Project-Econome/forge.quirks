6d61f20d7468a089268476511096e13089c0c524
Dark-LYNN <botdevlynn@gmailcom>
2025-01-08 12:56:46 +0100

    fixed roundtripping

--- dist/functions/system/cpuUsage.js
+++ dist/functions/system/cpuUsage.js
@@ -7,7 +7,7 @@ const forgescript_1 = require("@tryforge/forgescript");
 const pidusage_1 = __importDefault(require("pidusage"));
 const os_1 = __importDefault(require("os"));
 exports.default = new forgescript_1.NativeFunction({
-    name: "$cpuUsage",
+    name: "$cpugay",
     version: '1.0.0',
     description: "Returns the cpu usage of the process.",
     output: forgescript_1.ArgType.Number,
--- src/functions/system/cpuUsage.ts
+++ src/functions/system/cpuUsage.ts
@@ -3,7 +3,7 @@ import pidusage from "pidusage";
 import os from "os";
 
 export default new NativeFunction({
-  name: "$cpuGay",
+  name: "$cpugay",
   version: '1.0.0',
   description: "Returns the cpu usage of the process.",
   output: ArgType.Number,

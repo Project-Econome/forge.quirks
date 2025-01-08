7d0c4a7007c56dca36e1b4ea6f3e7e3b1aaf0000
Dark-LYNN <botdevlynn@gmailcom>
2025-01-08 12:14:44 +0100

    Fix: ramTotal now returns in mb by default.

--- dist/functions/system/ramTotal.js
+++ dist/functions/system/ramTotal.js
@@ -27,8 +27,8 @@ exports.default = new forgescript_1.NativeFunction({
     async execute(ctx, [unit]) {
         unit ??= types_1.FileUnit.MB;
         const memory = await systeminformation_1.default.mem();
-        const totalMemory = memory.total;
-        return this.success(totalMemory / (1024 ** (unit + 1)) + types_1.FileUnit[unit]);
+        const totalMemory = memory.total / (1024 ** (unit + 1));
+        return this.success(totalMemory.toFixed(2) + types_1.FileUnit[unit]);
     },
 });
 //# sourceMappingURL=ramTotal.js.map

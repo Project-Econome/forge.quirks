5f80455c58332d398661fd6b277007ba078b189a
Dark-LYNN <botdevlynn@gmailcom>
2025-01-09 13:54:55 +0100

    UWU

--- dist/functions/system/cpuThreads.js
+++ dist/functions/system/cpuThreads.js
@@ -6,10 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const forgescript_1 = require("@tryforge/forgescript");
 const systeminformation_1 = __importDefault(require("systeminformation"));
 exports.default = new forgescript_1.NativeFunction({
-    name: "$cpuCores",
+    name: "$cpuThreads",
     version: "1.0.7",
     output: forgescript_1.ArgType.Number,
-    description: "Returns the amount of cpu cores",
+    description: "Returns the amount of cpu Threads",
     unwrap: false,
     async execute(ctx) {
         const cpu = await systeminformation_1.default.cpu();

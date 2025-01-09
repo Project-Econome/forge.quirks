08e91574b1ff66e83ddf0c0b78061449bceac0f5
Dark-LYNN <botdevlynn@gmailcom>
2025-01-09 13:32:25 +0100

    feat: Overwrite cpuCores to show the actual core amount.

---
+++ src/functions/system/cpuCores.ts
@@ -0,0 +1,14 @@
+import { ArgType, NativeFunction, Return } from "@tryforge/forgescript"
+import si from "systeminformation";
+
+export default new NativeFunction({
+  name: "$cpuCores",
+  version: "1.0.7",
+  output: ArgType.Number,
+  description: "Returns the amount of cpu cores",
+  unwrap: false,
+  async execute(ctx) {
+    const cpu = await si.cpu()
+    return this.success(cpu.physicalCores)
+  },
+})

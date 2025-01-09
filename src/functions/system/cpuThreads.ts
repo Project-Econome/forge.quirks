d5ebc8ce4bd57d63c6e31a4c6f9cc418e4fabfa1
Dark-LYNN <botdevlynn@gmailcom>
2025-01-09 13:50:57 +0100

    fix: cpuThreads and cpuCores had same name.

--- src/functions/system/cpuThreads.ts
+++ src/functions/system/cpuThreads.ts
@@ -2,13 +2,13 @@ import { ArgType, NativeFunction, Return } from "@tryforge/forgescript"
 import si from "systeminformation";
 
 export default new NativeFunction({
-  name: "$cpuCores",
+  name: "$cpuThreads",
   version: "1.0.7",
   output: ArgType.Number,
-  description: "Returns the amount of cpu cores",
+  description: "Returns the amount of cpu Threads",
   unwrap: false,
   async execute(ctx) {
     const cpu = await si.cpu()
     return this.success(cpu.cores)
   },
-}) 
+})
